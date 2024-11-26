12ee7664055a23a10d2aab15fa3285a302755fb4
walid lhaila <walidlhaila00@gmail.com>
2024-11-26 20:34:06 +0100

    Feat: INSTALL TAILWIND

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+function Home () {
+    return (
+
+        <>
+            <nav>
+                <h1 className="text-red-600">E-SPORTIF</h1>
+            </nav>
+        </>
+
+    );
+}
+
+
+export default Home;
+
+
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+
+
+function AppRoutes () {
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+
+    );
+}
+
+
+export default AppRoutes;
